cdc7a6f116c7e12647d5795315152cb2944fb3a7
pramod <pramod.r@vrinsoft.com>
2022-06-09 21:06:25 +0530

    file structure change and bug fixing

--- advanced-all-in-one-forms.php
+++ advanced-all-in-one-forms.php
@@ -9,10 +9,13 @@
  * Text Domain : advanced-all-in-one-forms
 */
 defined( 'ABSPATH' ) OR exit;
-register_activation_hook(   __FILE__, array( 'All_Advanced_Form_Class', 'on_activation' ) );
-register_deactivation_hook( __FILE__, array( 'All_Advanced_Form_Class', 'on_deactivation' ) );
-register_uninstall_hook(    __FILE__, array( 'All_Advanced_Form_Class', 'on_uninstall' ) );
-add_action( 'plugins_loaded', array( 'All_Advanced_Form_Class', 'init' ) );
-if(!class_exists( 'All_Advanced_Form_Class')){
-    require_once('All_Advanced_Form_Class.php');
+register_activation_hook(   __FILE__, array( 'AI_Alladvancedform', 'on_activation' ) );
+register_deactivation_hook( __FILE__, array( 'AI_Alladvancedform', 'on_deactivation' ) );
+register_uninstall_hook(    __FILE__, array( 'AI_Alladvancedform', 'on_uninstall' ) );
+add_action( 'plugins_loaded', array( 'AI_Alladvancedform', 'init' ) );
+if(!defined('AI_ADVANCE_FORM_URL')) {
+    define('AI_ADVANCE_FORM_URL', plugin_dir_url( __FILE__ ));
+}
+if(!class_exists( 'AI_Alladvancedform')){
+    require_once('includes/class-ai-alladvancedform.php');
 }
